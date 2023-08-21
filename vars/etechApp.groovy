8db55edd89f7829007499bb7e39eacbec775c22f
Samuel Ukaegbu <sukaegbu2003@yahoo.com>
2023-08-21 04:45:51 -0500

    modified the groovy file
    Signed-off-by: Samuel Ukaegbu <sukaegbu2003@yahoo.com>

--- vars/etechApp.groovy
+++ vars/etechApp.groovy
@@ -1,37 +1,37 @@
 def (String repourl){
     pipeline{
-    agent any
-    stages{
-        stage('1-clone'){
-            steps{
-               sh 'cat /etc/passwd'
-            }
-        }
-        stage('2-parallel-jobs'){
-            parallel{
-                stage('1-subjob1'){
-                    steps{
-                        sh 'lscpu'
-                    }
+        agent any
+        stages{
+            stage('1-clone'){
+                steps{
+                    sh 'cat /etc/passwd'
                 }
-                stage('2-subjob2'){
-                    steps{
-                        sh 'df -h'
+            }
+            stage('2-parallel-jobs'){
+                parallel{
+                    stage('1-subjob1'){
+                        steps{
+                            sh 'lscpu'
+                        }
                     }
-                }          
+                    stage('2-subjob2'){
+                        steps{
+                            sh 'df -h'
+                        }
+                    }          
+                }
             }
-        }
-        stage('3-codetest'){
-            steps{
-                sh 'free -m'
+            stage('3-codetest'){
+                steps{
+                    sh 'free -m'
+                }
             }
-        }
-        stage('4-clossing'){
-            steps{
-                echo "we are done "
+            stage('4-clossing'){
+                steps{
+                    echo "we are done "
+                }
             }
         }
     }
-}
 
 }
